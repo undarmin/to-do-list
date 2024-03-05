d4957ff252ee8e3b39ec6604c4a4fca271450a0c
undarmin <tejasondg@Gmail.com>
2024-03-05 11:59:18 +0530

    Add projects (project class not made yet)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import Task from "./task.js";
+import Project from './project.js';
 import DOM from "./dom.js";
 
-DOM.taskModalComponents.submitButton.addEventListener("click", (e) => {
+const defaultProject = new Project("default", "Default");
+const projects = {
+  all: [],
+};
+
+projects.active = defaultProject;
+DOM.setProjectNode(projects.active);
+
+const defprojnode = DOM.createProjectNode(projects.active);
+DOM.appendProjectNode(defprojnode);
+
+function createTask(e) {
   const formData = DOM.submitTaskData(e);
   if (!formData) {
     return;
@@ -10,8 +22,16 @@ DOM.taskModalComponents.submitButton.addEventListener("click", (e) => {
     formData.title, formData.description,
     formData.duedate, formData.priority
   )
-  const taskNode = DOM.createTaskInDOM(task);
+  projects.active.addTask(task);
+  const taskNode = DOM.createTaskInDOM(task, defprojnode);
+  projects.active.addTaskNode(taskNode);
+}
+
+DOM.taskModalComponents.submitButton.addEventListener("click", (e) => {
+  createTask(e)
 });
 
 const st = new Task("m", "m", "m/m/m", "9");
-const stn = DOM.createTaskInDOM(st);
+const stn = DOM.createTaskInDOM(st, defprojnode);
+projects.active.addTask(st);
+projects.active.addTaskNode(stn);
