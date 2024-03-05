6dbe0a627af6595568e0e2bcaf6ad4d6a1f3c653
undarmin <tejasondg@Gmail.com>
2024-03-05 12:49:44 +0530

    Set up appropriate event listeners

--- src/index.js
+++ src/index.js
@@ -2,19 +2,44 @@ import Task from "./task.js";
 import Project from './project.js';
 import DOM from "./dom.js";
 
-const defaultProject = new Project("default", "Default");
+const defaultProject = new Project("Default");
 const projects = {
   all: [],
 };
 
-projects.active = defaultProject;
-DOM.setProjectNode(projects.active);
+const projectNodes = {
+  all: [],
+}
 
+projects.active = defaultProject;
 const defprojnode = DOM.createProjectNode(projects.active);
+projects.all.push(defaultProject);
+DOM.setProjectNode(projects.active);
+projectNodes.active = defprojnode;
+projectNodes.all.push(defprojnode);
+
 DOM.appendProjectNode(defprojnode);
 
+function createProject(e) {
+  const formData = DOM.submitData(e, DOM.projectModal, DOM.projectModalComponents);
+  if (!formData) {
+    return;
+  }
+  const project = new Project(formData.name);
+  
+  projects.all.push(project);
+  const projectNode = DOM.createProjectNode(project);
+  DOM.appendProjectNode(projectNode);
+  if (formData.setActive === "on") {
+    projects.active = project;
+    DOM.setProjectNode(projects.active);
+    projectNodes.active = projectNode;
+  }
+
+}
+
 function createTask(e) {
-  const formData = DOM.submitTaskData(e);
+  const formData = DOM.submitData(e, DOM.taskModal, DOM.taskModalComponents);
   if (!formData) {
     return;
   }
@@ -23,9 +48,14 @@ function createTask(e) {
     formData.duedate, formData.priority
   )
   projects.active.addTask(task);
-  const taskNode = DOM.createTaskInDOM(task, defprojnode);
+  const taskNode = DOM.createTaskInDOM(task, projectNodes.active);
   projects.active.addTaskNode(taskNode);
 }
+
+DOM.projectModalComponents.submitButton.addEventListener('click', e => {
+  createProject(e);
+})
+
 
 DOM.taskModalComponents.submitButton.addEventListener("click", (e) => {
   createTask(e)
